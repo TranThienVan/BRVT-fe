8ef582a393f60621b076c6d75da0e20f743a5e61
TranThienVan <s3740845@rmit.edu.vn>
2021-12-17 22:51:14 +0700

    Adding Services, BookingPage, About

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Icon from "./Components/Icon/Icon";
 import Navbar from "./Components/Navbar/Navbar";
 import Service from "./Components/Service/Service";
+import About from "./Pages/About/About";
+import BookingPage from "./Pages/BookingPage/BookingPage";
 import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <HomePage />
       <Icon />
       <Service />
+      <About />
+      <BookingPage />
     </div>
   );
 }
